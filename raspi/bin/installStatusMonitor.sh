71d60e952f4ca2e25c1523e2d2fe506c46daa786
Lorenz <lorenz.hahn@actano.de>
2019-04-10 15:05:22 +0200

    add install script

---
+++ raspi/bin/installStatusMonitor.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+XAUTOSTART=/home/pi/.config/lxsession/LXDE-pi/autostart
+
+touch $XAUTOSTART
+chmod 755 $XAUTOSTART
+
+<<EOF >> $XAUTOSTART
+#!/bin/sh
+/home/pi/monitoring-display/raspi/bin/startChrome.sh
+EOF
