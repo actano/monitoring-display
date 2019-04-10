72f5bea7c80ddff66e952d2177aaa14263db4e2b
Lorenz Hahn <lorenz.hahn@actano.de>
2019-04-10 15:09:28 +0200

    Fix install script

--- raspi/bin/installStatusMonitor.sh
+++ raspi/bin/installStatusMonitor.sh
@@ -5,7 +5,7 @@ XAUTOSTART=/home/pi/.config/lxsession/LXDE-pi/autostart
 touch $XAUTOSTART
 chmod 755 $XAUTOSTART
 
-<<EOF >> $XAUTOSTART
+cat << EOF >> $XAUTOSTART
 #!/bin/sh
 /home/pi/monitoring-display/raspi/bin/startChrome.sh
 EOF
